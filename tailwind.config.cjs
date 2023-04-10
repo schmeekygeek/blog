41b7cc37b4cc811d1cb4f45eedbd1da0951862d8
schmeekygeek <ss6087982@gmail.com>
2023-04-10 20:14:19 +0530

    update website info

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  darkMode: 'class',
 	theme: {
-		extend: {},
+		extend: {
+      fontFamily: {
+        satoshi: ["'Satoshi'", 'Verdana', "sans-serif"]
+      }
+    },
 	},
 	plugins: [],
 }
