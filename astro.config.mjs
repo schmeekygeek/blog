3ce61c9b8be4c01cd60c784977689ec6530ac595
schmeekygeek <ss6087982@gmail.com>
2023-04-11 10:46:51 +0530

    light/dark theme and banner

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://www.schmeeky.dev',
   integrations: [sitemap(), react(), image(), tailwind()]
 });
