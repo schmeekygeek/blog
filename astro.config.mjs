f6c22da67497ef35f0059c9e56b7bc98068fc285
schmeekygeek <ss6087982@gmail.com>
2023-04-14 21:00:41 +0530

    stable build

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,22 +10,24 @@ export default defineConfig({
   site: 'https://schmeeky.pages.dev',
   integrations: [sitemap(), react(), image(), tailwind()],
   markdown: {
-    extendDefaultPlugins: true,
-    rehypePlugins: [
-      [
-        addClasses,
-        {
-          h1: 'text-rosePine-text text-4xl font-satoshi font-semibold',
-          h2: 'text-2xl font-satoshi font-semibold',
-          h3: 'text-xl font-satoshi font-semibold',
-          h4: 'text-lg font-satoshi font-semibold',
-          h5: 'font-satoshi font-semibold',
-          h6: 'font-satoshi font-semibold',
-          img: 'border border-slate-300 dark:border-zinc-700 rounded-xl mb-6',
-          p: 'mb-6',
-          a: 'underline underline-offset-2 hover:text-orange-500 decoration-orange-500'
-        }
-      ]
-    ]
+    extendDefaultPlugins: true
+//    rehypePlugins: [
+//      [
+//        addClasses,
+//        {
+//          h1: 'mb-4 underline underline-offset-2 dark:text-rosePine-text text-rosePineDawn-text decoration-rosePine-gold text-4xl',
+//          h2: 'mb-4 underline underline-offset-2 dark:text-rosePine-text text-rosePineDawn-text decoration-rosePine-gold text-2xl',
+//          h3: 'mb-4 underline underline-offset-2 dark:text-rosePine-text text-rosePineDawn-text decoration-rosePine-gold text-xl',
+//          h4: 'mb-4 underline underline-offset-2 dark:text-rosePine-text text-rosePineDawn-text decoration-rosePine-gold text-lg',
+//          h5: 'mb-4 underline underline-offset-2 dark:text-rosePine-text text-rosePineDawn-text decoration-rosePine-gold text-lg',
+//          h6: 'font-satoshi font-semibold',
+//          img: 'border border-slate-300 dark:border-zinc-700 rounded-xl mb-6',
+//          p: 'mb-6',
+//          a: 'underline underline-offset-2 hover:text-orange-500 decoration-orange-500',
+//          ul: 'list-disc',
+//          li: 'list-disc'
+//        }
+//      ]
+//    ]
   }
 });
